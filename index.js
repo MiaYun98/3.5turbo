4a4c7d3d674ac882b243f8b0c9ff770d23549657
Mia <113086524+MiaYun98@users.noreply.github.com>
2023-05-17 11:10:04 -0700

    .env function added

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ import { Configuration, OpenAIApi } from "openai";
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const configuration = new Configuration({
-
+    organization: process.env.ORGANIZATION,
+    apiKey: process.env.OPENAI_API_KEY
 });
 
 const openai = new OpenAIApi(configuration);
